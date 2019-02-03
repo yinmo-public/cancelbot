97bee434c17605af30e55c939dcd31b65251368a
尹莫 <yinmo.contact@gmail.com>
2019-02-03 15:59:05 +0800

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,35 @@
+from Cancel.linepy import *
+from Cancel.akad.ttypes import *
+from time import sleep
+import time
+
+yinmo = LINE("cancelgroup@yandex.com","NBA2k190")
+oepoll = OEPoll(yinmo)
+
+def NOTIFIED_INVITE_INTO_GROUP(op):
+    try:
+        yinmo.acceptGroupInvitation(op.param1)       
+        group = yinmo.getGroup(op.param1)
+        if group.invitee is None:
+            yinmo.kickoutFromGroup(op.param1, [op.param2])
+            yinmo.leaveGroup(op.param1)
+        else:
+            group = yinmo.getGroup(op.param1)
+            groupinvitingmembersmid = [contact.mid for contact in group.invitee]
+            for _mid in groupinvitingmembersmid:
+                yinmo.cancelGroupInvitation(op.param1, [_mid])
+                time.sleep(0.2)
+            yinmo.leaveGroup(op.param1)
+    except Exception as e:
+        print(e)
+        print("\n\nNOTIFIED_INVITE_INTO_GROUP\n\n")
+        return
+
+
+oepoll.addOpInterruptWithDict({
+    OpType.NOTIFIED_INVITE_INTO_GROUP: NOTIFIED_INVITE_INTO_GROUP
+})
+
+while True:
+    oepoll.trace()
+    
